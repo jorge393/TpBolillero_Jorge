650e242c7fb67a0a4eb853460b62ebdb6cf3f7ba
jorge393 <83706288+jorge393@users.noreply.github.com>
2022-05-21 16:35:37 -0300

    update Simulacion.cs

--- tpbolillero.core/Simulacion.cs
+++ tpbolillero.core/Simulacion.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace tpbolillero.core
 {
@@ -10,16 +11,20 @@ namespace tpbolillero.core
         {
             return bolillero.JugarN(jugadas,num2);
         }
-        public long SimularConHilos(Bolillero bolillero,long numtareas,List<byte> jugadas, long num2)
+        public long SimularConHilos(Bolillero bolillero,long numtareas,List<byte> jugadas, long hilosenuso)
         {
             
-            Task<long>[] tareas = new Task<long>[numtareas];
-            for (int i = 1; i < num2; i++)
+            Task<long>[] tareas = new Task<long>[hilosenuso];
+            long suma = 0; 
+            for (int i = 1; i < numtareas; i++)
             {
-                
+                Bolillero clon = (Bolillero)bolillero.Clone();
+                tareas[i] = Task<long>.Run(() => SimularSinHilos(clon, jugadas, numtareas));
+                Task<long>.WaitAll(tareas);
+                tareas.Sum(suma + tareas.Result);
             }
-
             return 1;
+            
         }
 
     }
